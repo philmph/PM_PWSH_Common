3993a5d892b62c6f7c440924cc9b3e66b78ad4a4
Philipp Maier <git@pmaier.at>
2021-08-10 09:44:21 +0200

    Update Get-ADDirectoryServerOperationMasterRole

--- Functions/Get-ADDirectoryServerOperationMasterRole.ps1
+++ Functions/Get-ADDirectoryServerOperationMasterRole.ps1
@@ -2,16 +2,16 @@ function Get-ADDirectoryServerOperationMasterRole {
     <#
     .SYNOPSIS
     Used to query fsmo owners.
-    
+
     .DESCRIPTION
     Used to query one or more domains for their fsmo owners.
-    
+
     .PARAMETER DomainName
     Defines one or more domainnames to query for fsmo owners. Defaults to env:USERDNSDOMAIN.
-    
+
     .EXAMPLE
     PS C:\> Get-ADDirectoryServerOperationMasterRole -DomainName 'pmaier.lab'
-    
+
 
     DomainName           : pmaier.lab
     DomainNamingMaster   : WIN-5LI9IQMANGJ.pmaier.lab
@@ -30,43 +30,44 @@ function Get-ADDirectoryServerOperationMasterRole {
     InfrastructureMaster : WIN-5LI9IQMANGJ.pmaier.lab
     PDCEmulator          : WIN-5LI9IQMANGJ.pmaier.lab
     RIDMaster            : WIN-5LI9IQMANGJ.pmaier.lab
-    
+
     .INPUTS
     [System.String]
-    
+
     .OUTPUTS
     [PSCustomObject]
-    
+
     .NOTES
-    Author:			Philipp Maier
-    Author Git:		https://github.com/philmph
-    
+    Author: Philipp Maier
+    Author Git: https://github.com/philmph
+
     .LINK
-    https://github.com/philmph/PWSH_Common_Functions
+    https://github.com/philmph/PWSH_Common_Functions/blob/main/Docs/Get-ADDirectoryServerOperationMasterRole.md
     #>
-    
+
     [CmdletBinding()]
-    
+
     param (
-        [Parameter(Mandatory=$false,
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=0)]
-        [ValidateScript({
-            # Check for domain name pattern
-            if ($_ -match '^[0-9a-z]+(\.[0-9a-z]+)+$') {
-                $true
-            } else {
-                throw "'$_' is not a valid domain name."
-            }
-        })]
+        [Parameter(
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName,
+            Position = 0
+        )]
+        [ValidateScript( {
+                # Check for domain name pattern
+                if ($_ -match '^[0-9a-z]+(\.[0-9a-z]+)+$') {
+                    $true
+                } else {
+                    throw "'$_' is not a valid domain name."
+                }
+            })]
         [string[]]$DomainName = $env:USERDNSDOMAIN
     ) # param
-    
+
     begin {
         Set-StrictMode -Version 2
     } # begin
-    
+
     process {
         # $Domain can only be empty when not defined by user and $env:USERDNSDOMAIN doesn't exist.
         # If defined ValidateScript would throw. Example Get-ADDirectoryServerOperationMasterRole -Domain ''
@@ -82,18 +83,18 @@ function Get-ADDirectoryServerOperationMasterRole {
                 $Domain = Get-ADDomain -Server $DomainObj
 
                 [PSCustomObject]@{
-                    DomainName = $DomainObj
-                    DomainNamingMaster = $Forest.DomainNamingMaster
-                    SchemaMaster = $Forest.SchemaMaster
+                    DomainName           = $DomainObj
+                    DomainNamingMaster   = $Forest.DomainNamingMaster
+                    SchemaMaster         = $Forest.SchemaMaster
                     InfrastructureMaster = $Domain.InfrastructureMaster
-                    PDCEmulator = $Domain.PDCEmulator
-                    RIDMaster = $Domain.RIDMaster
+                    PDCEmulator          = $Domain.PDCEmulator
+                    RIDMaster            = $Domain.RIDMaster
                 }
             } catch {
                 Write-Error -Exception Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException -Message "$DomainName doesn't exist or cannot be reached."
             } # try/catch
         } # foreach
     } # process
-    
+
     end {} # end
 } # function
